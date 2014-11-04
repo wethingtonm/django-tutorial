30576fc6f3542fb13f307097433d0aae113ab108
Marshall Wethington <marshall.wethington@gmail.com>
2014-11-03 18:16:52 -0600

    creating the polls app

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
-	question_text = models.CharField(max_length=200
-	pub_date = models.DateTimeField('date published
+	question_text = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
 
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question)
-	choice_text = models.CharField(max_length=200
-	votes = models.InterField(default=0)
+	choice_text = models.CharField(max_length=200)
+	votes = models.IntegerField(default=0)
 	
